c5e6d74829afd8891e22d4bd7cd4630b8acf7489
DukeofWaterloo <59591006+DukeofWaterloo@users.noreply.github.com>
2020-12-01 00:02:20 -0500

    Make some good progress
    For the most part, this code actually works! For the future just numbers/symbols and making sure the user input is verified!

--- pyDotDash.py
+++ pyDotDash.py
@@ -15,7 +15,7 @@ def morseToAlphabet(message):
                     translation += alphabet[k]
         translation += " "
 
-    print(translation)
+    return translation.lstrip(" ")
 
 #alphabetToMorse takes in a english decrypted message and returns an encrypted morse message
 def alphabetToMorse(message):
@@ -31,25 +31,117 @@ def alphabetToMorse(message):
             translation += " "
         translation += "   "
 
-    print(translation)
+    return translation.lstrip(" ")
 
 #printOut displays an alphabet : morse chart to help out users
 def printOut():
     block = ("------------------------")
-    print(block)
     for i in range(26):
         print(alphabet[i] + " : " + morse[i])
     print(block)
+
+
+def menu():
+    run = 1
+    choice = ""
+    block = ("------------------------")
+    print("Hello there and welcome to the pyDotDash Morse/English translator!\n")
+    while(run==1): 
+        print("Would you like to...") 
+        print("See an Alphabet : Morse [C]hart")
+        print("Write a message in [M]orse")
+        print("Write a message [E]nglish?")
+        print("or e[X]it?")
+        print(block)
+        choice = input("").upper()
+        if (choice == "C"):
+            printOut()
+
+        elif (choice == "M"):
+            print("How would you like to enter your message?\n")
+            print("1) [K]eyboard")
+            inputMethod = input("").upper()
+
+            print ("How would you like to output your message?\n")
+            print("1) [C]onsole")
+            outputMethod = input("").upper()
+
+            if (inputMethod == "K"):
+                print("Please enter your message: (Format is one space between characters and three spaces between words)")
+                morseMessage = input("")
+                englishMessage = morseToAlphabet(morseMessage)
+
+                if (outputMethod == "C"): 
+                    print("Your translated message is: ")
+                    print(englishMessage)
+
+            print("Would you like to run pyDotDash again? ([Y]/[N])")
+            repeat = input("").upper()
+
+            if (repeat == "Y"):
+                print("")
+
+            elif (repeat =="N"):
+                print("EXITING PROGRAM")
+                run = 0
+            else:
+                print("INVALID ENTRY DETECTED, EXITING PROGRAM")
+                run = 0
+
+
+
+
+        elif (choice == "E"):
+            print("How would you like to enter your message?\n")
+            print("1) [K]eyboard")
+            inputMethod = input("").upper()
+
+            print("How would you like to output your message?\n")
+            print("1) [C]onsole")
+            outputMethod = input("").upper()
+
+            if (inputMethod == "K"):
+                print("Please enter your message: ")
+                englishMessage = input("")
+                morseMessage = alphabetToMorse(englishMessage)
+
+                if (outputMethod == "C"): 
+                    print("Your translated message is: ")
+                    print(morseMessage)
+
+            print("Would you like to run pyDotDash again? ([Y]/[N])")
+            repeat = input("").upper()
+
+            if (repeat == "Y"):
+                print("")
+
+            elif (repeat =="N"):
+                print("EXITING PROGRAM")
+                run = 0
+            else:
+                print("INVALID ENTRY DETECTED, EXITING PROGRAM")
+                run = 0
+
+
+        elif (choice == "X"):
+            print("EXITING PROGRAM")
+            quit()
+
+        else:
+            print("Invalid entry, please choose one of the options mentioned")
+    
+
+
+
 
 #Sample Messages for Testing Purposes
 msg = "HELLO THERE!"
 mmsg = ".... . .-.. .-.. ---   - .... . .-. ."
 
-printOut()
-morseToAlphabet(mmsg)
-print("- - - - - - - - -")
-alphabetToMorse(msg)
+#printOut()
+#morseToAlphabet(mmsg)
+#print("- - - - - - - - -")
+#alphabetToMorse(msg)
 
-
-
+menu()
 
